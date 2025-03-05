b84ae2801a760a995e95feba61ba15c82f300ffe
DevRafaSilva <developerrafasilva@gmail.com>
2025-03-05 12:48:01 -0300

    Commit post

--- src/Pages/Topicos/Topicos.jsx
+++ src/Pages/Topicos/Topicos.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import CardPost from '../../Componentes/CardPostagens/CardPost';
 import Chips from '../../Componentes/Chips/Chips';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -22,41 +21,48 @@ const Topicos = () => {
     }
     getPosts();
   }, []);
-  return (
-    <>
-      <Box sx={{ px: 7, py: 4 }} component="main">
-        <Chips text="Passáros" />
-        <Chips text="Selva" />
-        <Chips text="Savana" />
-        <Box
-          sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '20px' }}
-        >
-          {blog &&
-            blog.map((item) => (
-              <Card
-                sx={{
-                  maxWidth: 345,
-                  mb: 4,
-                }}
-              >
-                <CardMedia
-                  sx={{ height: 140 }}
-                  image="/leao.jpg"
-                  title="Imagem do post"
-                />
-                <CardContent>
-                  <h3>{item.post_title}</h3>
-                  <p style={{ margin: '20px 0' }}>{item.post_content}</p>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">VER PUBLICAÇÃO</Button>
-                </CardActions>
-              </Card>
-            ))}
+  if (blog) {
+    return (
+      <>
+        <Box sx={{ px: 7, py: 4 }} component="main">
+          <Chips text="Passáros" />
+          <Chips text="Selva" />
+          <Chips text="Savana" />
+          <Box
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr',
+              gap: '20px',
+            }}
+          >
+            {blog &&
+              blog.map((item, index) => (
+                <Card
+                  key={index}
+                  sx={{
+                    maxWidth: 345,
+                    mb: 4,
+                  }}
+                >
+                  <CardMedia
+                    sx={{ height: 140 }}
+                    image="/leao.jpg"
+                    title="Imagem do post"
+                  />
+                  <CardContent>
+                    <h3>{item.post_title}</h3>
+                    <p style={{ margin: '20px 0' }}>{item.post_content}</p>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">VER PUBLICAÇÃO</Button>
+                  </CardActions>
+                </Card>
+              ))}
+          </Box>
         </Box>
-      </Box>
-    </>
-  );
+      </>
+    );
+  } else return null;
 };
 
 export default Topicos;
